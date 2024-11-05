9ef29ab237f65adbad2f37c85a47bafaf31b032a
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-11-05 22:40:48 +0800

    added first page handler

---
+++ ex00/README.md
@@ -0,0 +1,5 @@
+```sh
+composer create-project symfony/skeleton:"^6.3" d04
+cd d04
+symfony server:start
+```
---
+++ ex00/d04/src/Controller/FirstPageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstPageController
+{
+    #[Route('/e00/firstpage', name: 'first_page')]
+    public function index(): Response
+    {
+        return new Response('Hello world!');
+    }
+}
